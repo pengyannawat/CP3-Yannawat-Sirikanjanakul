cc4151af5da39b21d82542b94ee204c2c32cd474
PENG <pengyannawat@users.noreply.github.com>
2020-04-18 17:11:21 +0700

    ใส่ข้อมูลคะแนนนักเรียน

---
+++ Assignments/Exercise4_Yannawat_S.py
@@ -0,0 +1,16 @@
+
+###สมมตินักเรียน A คะแนนแต่ละวิชาเป็นดังนี้
+FoundEngScore = 80
+GeneralBusScore = 75.6
+ComSystemScore = 95
+ComProgramScore = 99.99
+
+
+
+
+print("----------- Your Score -----------")
+print("Foundation English :", FoundEngScore)
+print("General Business :", GeneralBusScore)
+print("Introduction to Computer Systems :", ComSystemScore)
+print("Computer Programming :", ComProgramScore)
+print("")
